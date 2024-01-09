348313f03d39ffad66278e3a2f14a42808f14f29
Arun raj <113260000+Arun-cloud-dev@users.noreply.github.com>
2024-01-09 10:07:33 +0530

    update

---
+++ basic.ts
@@ -0,0 +1,115 @@
+// primitives: number , string , boolian 
+
+//  more comples types : array , objects 
+
+
+// function types , parameters 
+
+
+
+
+//primitives 
+
+let age:number = 24;
+
+age = 12.2;
+
+let userName: string | string[]; 
+userName = "Arun"
+
+
+let  isInstructor : boolean;
+isInstructor = true;
+
+
+
+//more conplex types array and objects 
+
+
+let hobbies : string[];
+
+hobbies = ['sports', "cooking"];
+
+
+type Person = {
+    name: string;
+    age:number;
+};
+
+
+type Aun = {
+    choose : boolean;
+}
+
+
+
+let tree:Person;
+
+tree = {
+    name: "neem",
+    age:5,
+    choose:true
+};
+
+
+// person = {
+//     isEmployee:true
+// }
+
+
+let people: {
+    name:string;
+    age:number;
+}[]; // array full of objects 
+
+
+
+
+
+// // type inference 
+
+// let cource  = "React - is good cource ";
+
+// working with union types 
+
+let cource: string | number | boolean  = "React - is good cource ";
+
+cource = 1512;
+
+
+// assigning type aliases 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
