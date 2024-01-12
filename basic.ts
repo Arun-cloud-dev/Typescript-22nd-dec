99f15b5ea720eef78073c2c22fb6ae40226b63f0
Arun raj <113260000+Arun-cloud-dev@users.noreply.github.com>
2024-01-12 23:58:29 +0530

    udate basic class and types in tS

--- basic.ts
+++ basic.ts
@@ -1,150 +1,188 @@
-// primitives: number , string , boolian 
+// // primitives: number , string , boolian 
 
-//  more comples types : array , objects 
-
-
-// function types , parameters 
+// //  more comples types : array , objects 
 
 
-
-
-//primitives 
-
-let age:number = 24;
-
-age = 12.2;
-
-let userName: string | string[]; 
-userName = "Arun"
-
-
-let  isInstructor : boolean;
-isInstructor = true;
+// // function types , parameters 
 
 
 
-//more conplex types array and objects 
+
+// //primitives 
+
+// let age:number = 24;
+
+// age = 12.2;
+
+// let userName: string | string[]; 
+// userName = "Arun"
 
 
-let hobbies : string[];
-
-hobbies = ['sports', "cooking"];
-
-
-type Person = {
-    name: string;
-    age:number;
-};
-
-
-type Aun = {
-    choose : boolean;
-}
+// let  isInstructor : boolean;
+// isInstructor = true;
 
 
 
-let tree:Person;
-
-tree = {
-    name: "neem",
-    age:5,
-    choose:true
-};
+// //more conplex types array and objects 
 
 
-// person = {
-//     isEmployee:true
+// let hobbies : string[];
+
+// hobbies = ['sports', "cooking"];
+
+
+// type Person = {
+//     name: string;
+//     age:number;
+// };
+
+
+// type Aun = {
+//     choose : boolean;
 // }
 
 
-let people: {
-    name:string;
-    age:number;
-}[]; // array full of objects 
+
+// let tree:Person;
+
+// tree = {
+//     name: "neem",
+//     age:5,
+//     choose:true
+// };
+
+
+// // person = {
+// //     isEmployee:true
+// // }
+
+
+// let people: {
+//     name:string;
+//     age:number;
+// }[]; // array full of objects 
 
 
 
 
 
-// // type inference 
+// // // type inference 
 
-// let cource  = "React - is good cource ";
+// // let cource  = "React - is good cource ";
 
-// working with union types 
+// // working with union types 
 
-let cource: string | number | boolean  = "React - is good cource ";
+// let cource: string | number | boolean  = "React - is good cource ";
 
-cource = 1512;
-
-
-// assigning type aliases 
+// cource = 1512;
 
 
-
-
-let person :[
-number, string 
-]
+// // assigning type aliases 
 
 
 
 
-
-// myArray :any[] = [
-//     {id:1, name: 'object 1'}
+// let person :[
+// number, string 
 // ]
 
 
-// addNewObject(){
-//     const newObj = {
-//         id: this.myArray.length +1 , name:"new object "
-//     };
-//     this.myArray.push(newObj);
+
+
+
+// // myArray :any[] = [
+// //     {id:1, name: 'object 1'}
+// // ]
+
+
+// // addNewObject(){
+// //     const newObj = {
+// //         id: this.myArray.length +1 , name:"new object "
+// //     };
+// //     this.myArray.push(newObj);
+// // }
+
+
+// interface MyObject {
+//     key : string;
+//     value : number;
 // }
 
 
-interface MyObject {
-    key : string;
-    value : number;
+// const myArray : MyObject[] = [
+//     {key :'item 1' , value :10},
+//     {key :'item 3' , value :40},
+//     {key :'item 4' , value :30}
+// ]
+
+
+
+// const newObj : MyObject = 
+//     {key: 'item4' , value:40};
+// ;
+
+// myArray.push(newObj);
+
+// console.log(myArray)
+
+
+
+// function add(a:number, b:number) {
+//  return a+b; 
+// } 
+
+// function print(value:any){
+//     console.log(value);
+// } 
+
+
+// // generics 
+
+// function insertAtBegining<T>(array:T[] , value:T){
+//     const newArray = [value,...array];
+//     return newArray;
+// }
+
+// const demoArray =[1,2,3];
+
+// const updatedArray = insertAtBegining(demoArray, -1); // [-1,1,2,3]
+// const stringArray = insertAtBegining(['a','b','c'],'d')
+
+
+// updatedArray[0].split('');
+
+
+// class and interfaces
+
+class Student {
+    firstname:string;
+    lastName:string;
+    age:number;
+    private courses:string[]; 
+
+constructor (public first:string , public last:string ,public age:number, private courses:string[]) {
+this.firstname= first; 
+this.lastName=last;
+this.age = age ;
+this.courses =courses;
+}
+ enrol(courceName:string){
+ this.courses.push(courceName);
+}
+
+listCourses(){
+return this.courses.slice();
+}
 }
 
 
-const myArray : MyObject[] = [
-    {key :'item 1' , value :10},
-    {key :'item 3' , value :40},
-    {key :'item 4' , value :30}
-]
+const student = new Student("Arun", "Raj", 23, ['angular']);
+
+student.enrol('React');
+student.courses();
 
 
-
-const newObj : MyObject = 
-    {key: 'item4' , value:40};
-;
-
-myArray.push(newObj);
-
-console.log(myArray)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+// student.courses => angular , react
 
 
 
